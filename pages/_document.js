eae6ec96991ca6fb0e6bd48032241a9f20302c07
Bruno Krebs <bruno.krebs@auth0.com>
2018-06-07 17:04:26 -0300

    fixing format;

--- pages/_document.js
+++ pages/_document.js
@@ -14,7 +14,7 @@ export default class MyDocument extends Document {
       <html>
       <Head>
         <title>The Auth0 Guest Author Program</title>
-        <meta charSet="utf-8" />
+        <meta charSet="utf-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <link rel="shortcut icon" href="https://cdn.auth0.com/styleguide/components/1.0.8/media/logos/img/favicon.png" />
         <link rel="stylesheet" href="./static/index.css" />
