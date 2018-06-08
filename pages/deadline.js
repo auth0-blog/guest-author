f25e4c386a183d34fd76776ea8cca57ebf28c710
Bruno Krebs <bruno.krebs@auth0.com>
2018-06-08 13:06:55 -0300

    rewording deadline;

--- pages/deadline.js
+++ pages/deadline.js
@@ -19,15 +19,14 @@ function Authorship(props) {
         cancelLabel="Go Back"
       >
         <p>
-          A frequently asked question is if there is a deadline for the process. The short answer is: no, the process
-          duration depends on how busy or free is the Guest Author during the process.
+          The process <strong>duration will vary</strong> depending on the topic and on how busy authors are during it.
         </p>
         <p>
-          The long answer is: guest authors usually manage to produce <strong>an article within two or three weeks</strong>.
-          However, we know that most guest authors have other duties and that they can get stuck with other important
-          matters. Therefore, it wouldn't be a problem if you take a little longer. Just don't end up with an article
-          unfinished that becomes old news.
+          Although most authors manage to produce <strong>an article within two or three weeks</strong>, Auth0 knows
+          that authors have other duties and that they can get stuck with other important matters. Therefore, it
+          won't be a problem if authors take a little longer.
         </p>
+        <p>Just be careful not to end up with an unfinished article that becomes old news.</p>
       </Presentation>
     </React.Fragment>
   );
