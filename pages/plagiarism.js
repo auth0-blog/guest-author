21af8873da5d0307c8ca96bb868d1d9d17180a0d
Bruno Krebs <bruno.krebs@auth0.com>
2018-06-08 12:24:55 -0300

    rewording plagiarism;

--- pages/plagiarism.js
+++ pages/plagiarism.js
@@ -19,17 +19,14 @@ function Authorship(props) {
         cancelLabel="Go Back"
       >
         <p>
-          Another extremelly important subject is regarding plagiarism. Auth0 takes advantage of modern plagiarism
-          checkers (like <a href="https://www.quetext.com/">Quetext</a> and <a href="https://www.grammarly.com/">
-          Grammarly</a>) to check articles. If we find evidence
-          that the article produced contains <strong>excerpts copied</strong> from existing resources without proper credits, we will
-          inform the author and <strong>terminate the process</strong>.
+          Plagiarism is an extremelly important subject. Authors must be aware that <strong>acting plagiarism is prohibited</strong>.
+          To protect itself, Auth0 uses modern plagiarism checkers like <a href="https://www.quetext.com/">Quetext </a>
+          and <a href="https://www.grammarly.com/">
+          Grammarly</a> to check articles.
         </p>
         <p>
-          This, of course, does not mean that authors cannot base their work on existing resources. Quite the contrary.
-          It is good to produce articles that are supported by previous work and an author can even
-          quote an external (or internal for that matter) reference. Authors just have to <strong>ensure</strong> that they are not
-          abusing on this practice and <strong>that the due credits are given</strong>.
+          Authors that copy excerpt from exist resources without adding credits, will no longer be able to participate
+          in the program.
         </p>
       </Presentation>
     </React.Fragment>
