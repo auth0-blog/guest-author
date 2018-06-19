db2ec65a05d890b6c355a16bf5c334ce15bccb12
Bruno Krebs <bruno.krebs@auth0.com>
2018-06-19 12:56:28 -0300

    playing around;

--- server.js
+++ server.js
@@ -10,20 +10,38 @@ app.prepare().then(() => {
   const server = express();
 
   server.use('/api', router);
+  //
+  // server.use('/guest-authors/onboard/static', express.static(__dirname + '/static', {
+  //   maxage: dev ? '0' : '365d',
+  // }));
+  //
+  // server.get('/guest-authors/onboard', (req, res) => {
+  //   const actualPage = `/${req.params['0']}`;
+  //   console.log(actualPage);
+  //   app.render(req, res, '/index');
+  // });
+  //
+  // server.get('/guest-authors/onboard/*', (req, res) => {
+  //   const actualPage = `/${req.params['0']}`;
+  //   console.log(actualPage);
+  //   app.render(req, res, actualPage);
+  // });
 
-  server.use('/guest-authors/onboard/static', express.static(__dirname + '/static', {
-    maxage: dev ? '0' : '365d',
-  }));
-
-  server.get('/guest-authors/onboard/*', (req, res) => {
-    const actualPage = `/${req.params['0']}`;
-    console.log(actualPage);
-    app.render(req, res, actualPage);
+  server.use('/_next/*', (req, res) => {
+    const newURL = req.originalUrl.replace('_next', 'admin/_next');
+    res.redirect(newURL);
   });
 
-  server.get('*', (req, res) => {
-    return handle(req, res);
+  server.use((req, res) => {
+    req.url = req.url.replace('/prefix', '');
+    req.url = req.url || '/';
+    console.log('======================', req.url || '/');
+    handle(req, res);
   });
+
+  // server.get('*', (req, res) => {
+  //   return handle(req, res);
+  // });
 
   server.listen(3000, (err) => {
     if (err) throw err;
